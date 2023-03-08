01ba8c1f091ecb439f2a4e8602dae22465f4abfc
Andrii-kyryliuk11 <poteriana@gmail.com>
2023-03-08 12:57:54 +0100

    Update FriendListItem.jsx

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
-  const status = isOnline === true ? "css.online" : "css.offline"
+  const status = isOnline === true ? css.online : css.offline
   
   return (
     <li className={css.item}>
